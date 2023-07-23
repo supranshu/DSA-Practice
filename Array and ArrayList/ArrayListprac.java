8cf9ec61cd7b96b5c64bbec9160dbb13b249bacd
SUPRANSHU SINGH <ss445198@gmail.com>
2023-07-23 15:21:30 +0530

    Arrays Completed

---
+++ Array and ArrayList/ArrayListprac.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListprac{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        list.add(0, 45);
+        list.add(1, 26);
+        list.add(2, 45);
+        list.add(3, 96);
+        list.add(4, 45);
+        System.out.println("Initial: "+list);
+        list.set(0, 99);
+        list.remove(2);
+        System.out.println("Updated: "+list); 
+        int get=list.get(2);
+        System.out.println("Item at index 2: "+get);
+
+    }
+}
--- Array and ArrayList/Arraysprac.java
+++ Array and ArrayList/Arraysprac.java
@@ -14,21 +14,85 @@ public class Arraysprac
 
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(rno2[0]);
-        System.out.println(arr[0]);
-        String[] arrin=new String[6];
-        System.out.println("Enter the string values");
-        for (int i=0;i<=5;i++)
-        {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println(rno2[0]);
+        // System.out.println(arr[0]);
+        // String[] arrin=new String[6];
+        // System.out.println("Enter the string values");
+        // for (int i=0;i<=5;i++)
+        // {
             
-            arrin[i]=sc.next();
+        //     arrin[i]=sc.next();
             
+        // }
+        // for (String string : arrin) //One of the best way to traverse is using fr each loop
+        // {
+        //     System.out.print(string+" ");
+        // } 
+        // System.out.println(Arrays.toString(arrin));//Best way to just traverse using the Arrays class
+
+        // int[][] arr2d={{1,2,3,4},{5,6},{7,8,9}};//here no. of coloumns is not fixed
+        // for(int row=0;row<arr2d.length;row++){
+        //     for(int col=0;col<arr2d[row].length;col++){
+        //         System.out.print(arr2d[row][col]);
+        //     }
+        //     System.out.println();
+        // }
+
+        // SWAPPING
+
+        int[] arr1={1,2,25,4,5};
+        swap(arr1,1,3);
+        System.out.println(Arrays.toString(arr1));
+
+
+        // MAXIMUM ITEM
+
+        System.out.println(max(arr1));
+
+        //  REVERSE
+
+        reverse(arr1);
+        System.out.println(Arrays.toString(arr1));
+       
+    }
+
+
+    private static void reverse(int[] arr) {
+
+        int start,end;
+        start=0;
+        end=arr.length-1;
+        while(start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
         }
-        for (String string : arrin) //One of the best way to traverse is using fr each loop
-        {
-            System.out.print(string+" ");
-        } 
-        System.out.println(Arrays.toString(arrin));//Best way to just traverse using the Arrays class
-    } 
+    }
+
+
+    private static int max(int[] arr) {
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            
+            if(arr[i]>max){
+                max=arr[i];
+            }
+
+        }
+        return max;
+    }
+
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+
+    
+    
+
+   
 }
