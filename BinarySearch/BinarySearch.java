2de554b46e2fbe783a139309849b8da03eb23102
SUPRANSHU SINGH <ss445198@gmail.com>
2023-07-25 22:34:14 +0530

    order agnostic

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -3,23 +3,32 @@ package BinarySearch;
 public class BinarySearch {
     public static void main(String[] args){
 
-        int[] arr={1,2,3,4,5,6,9};
-        binarySearchAlgo(arr,6);
+        int[] arr={9,8,7,6,5,4,3,2,1};
+        int order=orderAgnostic(arr);
+        if(order==1){
+            int index=reverseBinarySearchAlgo(arr,1);
+            System.out.println("Element found at: "+index);
+        }
+        else{
+            int index=normalBinarySearchAlgo(arr,1);
+            System.out.println("Element found at: "+index);
+        }
+        
 
     }
-    static boolean binarySearchAlgo(int[] arr,int target){
+    static int normalBinarySearchAlgo(int[] arr,int target){
         int start=0,end=arr.length-1;
         if(start==0&&end==0){
-                System.out.println("Array is emmpty");
-                return false;
+                
+                return -1;
             }
         while(start<=end)
         {
             int mid=start+(end-start)/2;
             
              if(target==arr[mid]){
-                System.out.println("Element found at index: "+mid);
-                return true;
+                
+                return mid;
             }
             else if(target<arr[mid]){
                 
@@ -31,11 +40,60 @@ public class BinarySearch {
                 start=mid+1;
             }
             else {
-                System.out.println("Element not found");
-                return false;
+               
+                return mid;
             }
         }
-        return false;
+
+        return -1;
+    }
+
+    static int reverseBinarySearchAlgo(int[] arr,int target){
+        int start=0,end=arr.length-1;
+        if(start==0&&end==0){
+                
+                return -1;
+            }
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            
+             if(target==arr[mid]){
+                
+                return mid;
+            }
+            else if(target<arr[mid]){
+                
+                
+                start=mid+1;
+
+            }
+            else if(target>arr[mid]){
+                
+                end=mid-1;
+                
+            }
+            else {
+               
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    static int orderAgnostic(int[] arr){
+
+        if(arr[0]>=arr[arr.length-1])
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+            
+        
     }
     
 }
